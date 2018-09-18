cb8c88cd1ce33e28ad5271f848cf9aeb5752f96f
Takafumi Arakaki <aka.tkf@gmail.com>
2018-09-17 17:07:23 -0700

    Fuzz _leak

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,33 @@
-using PyVirtualenv
+module TestPyVirtualenv
+
+using PyVirtualenv: _leak
+
 @static if VERSION < v"0.7.0-DEV.2005"
     using Base.Test
 else
     using Test
 end
 
-# write your own tests here
-@test 1 == 2
+N = 10000
+
+@testset "_leak(Cstring, ...)" begin
+    for i in 1:N
+        x = String(rand('A':'z', rand(1:1000)))
+        y = Base.unsafe_string(_leak(Cstring, x))
+        @test x == y
+    end
+end
+
+@testset "_leak(Cwstring, ...)" begin
+    for i in 1:N
+        x = String(rand('A':'z', rand(1:1000)))
+        a = Base.cconvert(Cwstring, x)
+        ptr = _leak(a)
+        z = unsafe_wrap(Array, ptr, size(a))
+        @test z[end] == 0
+        y = transcode(String, z)[1:end-1]
+        @test x == y
+    end
+end
+
+end  # module
